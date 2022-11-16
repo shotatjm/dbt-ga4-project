faedae7ba39460c00b76a7fcd60e747c0148a163
Shota Tajima <shota.tjm@streets.jp>
2022-11-16 23:28:19 +0900

    add warehouse_users

---
+++ models/warehouse/ga/whs_ga__users.sql
@@ -0,0 +1,53 @@
+WITH
+  target_term AS (
+    SELECT
+      event_occured_date,
+      target_date
+    FROM
+      UNNEST(GENERATE_DATE_ARRAY(PARSE_DATE('%Y%m%d', {{ var('ga_start_date') }}), CURRENT_DATE('Asia/Tokyo'))) AS event_occured_date,
+      UNNEST(GENERATE_DATE_ARRAY(event_occured_date, DATE_ADD(event_occured_date, INTERVAL 27 day))) AS target_date
+  ),
+  user_performance AS (
+    SELECT
+      user_pseudo_id,
+      event_date AS event_occured_date,
+      MAX(user_id) AS user_id,
+      COUNT(1) AS sessions,
+      SUM(page_views) AS page_views,
+      SUM(articles) AS articles,
+      SUM(read_to_ends) AS read_to_ends,
+    FROM
+      {{ ref('whs_ga__sessions') }}
+    GROUP BY
+      1,
+      2
+  ),
+  joined AS (
+    SELECT
+      user_pseudo_id,
+      target_date AS event_date,
+      MAX(user_id) AS user_id,
+      COUNT(DISTINCT event_occured_date) AS frequency,
+      COUNT(1) AS sessions_last_4_weeks,
+      SUM(page_views) AS page_views_last_4_weeks,
+      SUM(articles) AS articles_last_4_weeks,
+      SUM(read_to_ends) AS read_to_ends_last_4_weeks,
+      LOGICAL_OR(event_occured_date = target_date) AS is_visited_on_the_day,
+      COUNTIF(event_occured_date = target_date) AS sessions,
+      SUM(IF(event_occured_date = target_date, page_views, 0)) AS page_views,
+      SUM(IF(event_occured_date = target_date, articles, 0)) AS articles,
+      SUM(IF(event_occured_date = target_date, read_to_ends, 0)) AS read_to_ends,
+    FROM
+      target_term
+      JOIN user_performance USING (event_occured_date)
+    WHERE
+      target_date <= CURRENT_DATE('Asia/Tokyo')
+    GROUP BY
+      1,
+      2
+  )
+SELECT
+  *,
+  {{ calc_frequency_segment('frequency') }} AS frequency_segment
+FROM
+  joined
