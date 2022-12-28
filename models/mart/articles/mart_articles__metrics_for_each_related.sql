2d661ffaa460a7b28e4e22333710a2cf42fab1b9
Shota Tajima <shota.tjm@streets.jp>
2022-12-28 16:37:59 +0900

    fix

--- models/mart/articles/mart_articles__metrics_for_each_related.sql
+++ models/mart/articles/mart_articles__metrics_for_each_related.sql
@@ -1,11 +1,11 @@
 SELECT
   daily_metrics_for_each_related.article_id,
   related_article_id,
-  ANY_VALUE(article_log.title) AS related_article_title,
-  ANY_VALUE(article_log.thumbnail) AS related_article_thumbnail,
-  ANY_VALUE(article_log.link) AS related_article_link,
-  ANY_VALUE(article_log.published_at) AS related_article_published_at,
-  ANY_VALUE(article_log.creator) AS related_article_creator,
+  ANY_VALUE(articles.title) AS related_article_title,
+  ANY_VALUE(articles.thumbnail) AS related_article_thumbnail,
+  ANY_VALUE(articles.link) AS related_article_link,
+  ANY_VALUE(articles.published_at) AS related_article_published_at,
+  ANY_VALUE(articles.creator) AS related_article_creator,
   SUM(clicks) AS clicks,
   SUM(clicks_related_link) AS clicks_related_link,
   SUM(clicks_related_link_next) AS clicks_related_link_next,
@@ -19,7 +19,7 @@ SELECT
   SUM(IF(is_1st_month, clicks, 0)) AS clicks_1st_month,
 FROM
   {{ ref('mart_articles__daily_metrics_for_each_related') }} AS daily_metrics_for_each_related
-  LEFT JOIN {{ ref('whs_cms__article_log') }} AS article_log ON daily_metrics_for_each_related.related_article_id = article_log.article_id
+  LEFT JOIN {{ ref('whs_cms__articles') }} AS articles ON daily_metrics_for_each_related.related_article_id = articles.article_id
 GROUP BY
   1,
   2;
