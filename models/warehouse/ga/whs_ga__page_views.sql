1cded4d9846528ab941ca4e65fc1e7ba18f3761a
Shota Tajima <shota.tjm@streets.jp>
2023-01-26 01:27:05 +0900

    make page_views partition-table

--- models/warehouse/ga/whs_ga__page_views.sql
+++ models/warehouse/ga/whs_ga__page_views.sql
@@ -1,3 +1,15 @@
+{{
+  config(
+    materialized = 'incremental',
+    incremental_strategy = 'insert_overwrite',
+    partition_by={
+      "field": "event_date",
+      "data_type": "date",
+    },
+    tags=["hourly"],
+  )
+}}
+
 WITH
   event_extracted AS (
     SELECT
@@ -6,6 +18,9 @@ WITH
       {{ ref('whs_ga__events') }}
     WHERE
       event_name = 'page_view'
+    {% if is_incremental() %}
+      AND event_date >= _dbt_max_partition
+    {% endif %}
   ),
   add_useful_columns AS (
     SELECT
@@ -34,6 +49,9 @@ WITH
           {{ ref('whs_ga__read_to_ends') }} AS read_to_end
         WHERE
           read_to_end.event_date = event_extracted.event_date
+        {% if is_incremental() %}
+          AND event_date >= _dbt_max_partition
+        {% endif %}
           AND read_to_end.page_view_id = event_extracted.page_view_id
       ) AS read_to_end,
     FROM
